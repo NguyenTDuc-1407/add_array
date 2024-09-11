cd531fe3b8e56e41caef3dd67f0b8fd68369c0f5
Duc.NT <311020ntd@gmail>
2024-09-11 09:15:34 +0700

    update

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿int[] arr = { 10, 4, 6, 7, 8, 6, 0, 0, 0, 0 };
+int[] arr1 = themPhanTuKhoiMang(arr, 6,1);
+// ham them phan tu trong mang
+static int[] themPhanTuKhoiMang(int[] arr, int x, int y)
+{
+    for (int i = arr.Length-1; i >=x; i--){
+        arr[i] = arr[i-1];
+    }
+    arr[x-1]=y;
+    return arr;
+}
+Console.WriteLine(string.Join(",",arr1));
